896f596bd16bb14e0684a69eb26ef148aa33e932
Teronvill <mamedowu@mail.ru>
2022-09-04 10:21:32 +0300

    path 4

--- js/script.js
+++ js/script.js
@@ -1,50 +1,52 @@
 "use strict";
 
-let noteId=0;
+let noteId = 0;
 loadNotes();
 //кнопки настроек
 let btns = document.querySelectorAll('.setting__btn');
-btns[0].addEventListener('click', () => {addNote();});
+btns[0].addEventListener('click', () => {
+    addNote();
+});
 btns[1].addEventListener('click', () => {
     for (let elem of elems) {
         elem.style.background = setRandColor(getRandNum(1, 4), getRandNum(1, 3), getRandNum(1, 359));
     }
 });
-btns[3].addEventListener('click', ()=>{
+btns[3].addEventListener('click', () => {
     document.querySelector('#r1').classList.toggle('hide');
 })
 btns[5].addEventListener('click', saveNotes);
 
 //Изменить кол-во колонок
-function changeColCount(){ 
-    let area=document.querySelector('.work-area'),
-    range=document.querySelector('#r1');
-    area.style.columnCount=range.value;
+function changeColCount() {
+    let area = document.querySelector('.work-area'),
+        range = document.querySelector('#r1');
+    area.style.columnCount = range.value;
     console.log(range.value);
 }
 //Показ панели
-function showPallet(){
+function showPallet() {
     this.previousElementSibling.classList.toggle('hide');
 }
-function update(color){
+
+function update(color) {
     let note;
-    if (!color.id){
-        note=document.querySelector('html');
-    }else 
-    {
-        note=color.id.parentElement;
+    if (!color.id) {
+        note = document.querySelector('html');
+    } else {
+        note = color.id.parentElement;
     }
-    note.style.background= color.toRGBAString();
+    note.style.background = color.toRGBAString();
 }
-function updateText(color){
+
+function updateText(color) {
     let note;
-    if (!color.id){
-        note=document.querySelector('html');
-    }else 
-    {
-        note=color.id.parentElement;
+    if (!color.id) {
+        note = document.querySelector('html');
+    } else {
+        note = color.id.parentElement;
     }
-    note.style.color= color.toRGBAString();
+    note.style.color = color.toRGBAString();
 }
 //Рандомное число
 function getRandNum(min, max) {
@@ -104,7 +106,8 @@ let saveImgs = document.querySelectorAll('.note__saved');
 for (let saved of saveImgs) {
     saved.addEventListener('click', saveNotes);
 }
-function cleanIcon(){
+
+function cleanIcon() {
     let saveImgs = document.querySelectorAll('.note__saved');
     for (let saved of saveImgs) {
         saved.classList.remove('note__notSaved');
@@ -134,21 +137,20 @@ function notSaved(el, text) {
 }
 
 // Добавить заметку
-function addNote(title='Заголовок',text = 'Введите текст заметки', date = currentDate(), favor = false, background, color) {
+function addNote(title = 'Заголовок', text = 'Введите текст заметки', date = currentDate(), favor = false, background, color) {
     let newNote = document.createElement('div');
     let workArea = document.querySelector('.work-area');
-    let curId='note'+noteId++;
+    let curId = 'note' + noteId++;
     newNote.classList.add('note');
     newNote.innerHTML = `<div id='${curId}' class="note__title" '><h2 class="note__title" contenteditable="true">${title}</h2><div class="note__delete img"></div></div><div class="note__text" contenteditable="true">${text}</div><div class="note__footer"><div class="note__date">${date}</div><div class="note__btns"><div class="note__pallet img colorPicker" data-jscolor="{onChange: 'update(this,this.id=${curId})',onInput: 'update(this,this.id=${curId})',alpha:1, value:'CCFFAA'}"></div><div class="note__text-color img colorPicker" data-jscolor="{onChange: 'updateText(this,this.id=${curId})',onInput: 'updateText(this,this.id=${curId})',alpha:1, value:'CCFFAA'}"></div><div class="note__favorit img"></div><div class="note__saved img"></div></div></div>`;
-    if (background){
-        newNote.style.background=background;
-     }
-     else {
-        newNote.style.background = setRandColor(getRandNum(1, 4), getRandNum(1, 3), getRandNum(1, 359)); 
-     }
-     if (color){
-        newNote.style.color=color;
-     }
+    if (background) {
+        newNote.style.background = background;
+    } else {
+        newNote.style.background = setRandColor(getRandNum(1, 4), getRandNum(1, 3), getRandNum(1, 359));
+    }
+    if (color) {
+        newNote.style.color = color;
+    }
     workArea.append(newNote);
     let del = newNote.querySelector('.note__delete');
     del.addEventListener('click', deleteNote);
@@ -159,13 +161,12 @@ function addNote(title='Заголовок',text = 'Введите текст з
     let saveImg = newNote.querySelector('.note__saved');
     saveImg.addEventListener('click', saveNotes);
     let favoritImg = newNote.querySelector('.note__favorit');
-    
+
     if (favor) {
         favoritImg.classList.add('note__favorit--active');
     }
     favoritImg.addEventListener('click', favorNote);
     let elems = document.querySelectorAll('.note');
-
 }
 
 //Текущая дата
@@ -185,10 +186,10 @@ function saveNotes() {
     cleanIcon();
     let arr = [];
     let notes = document.querySelectorAll('.note');
-    let settings={};
-    settings.background=document.querySelector('html').style.backgroundColor;
-    settings.title=document.querySelector('h1').innerHTML;
-    settings.font=document.querySelector('html').style.color;
+    let settings = {};
+    settings.background = document.querySelector('html').style.backgroundColor;
+    settings.title = document.querySelector('h1').innerHTML;
+    settings.font = document.querySelector('html').style.color;
     console.log(settings);
     arr.push(settings);
     for (let note of notes) {
@@ -196,13 +197,13 @@ function saveNotes() {
         obj.title = note.querySelector('h2').innerHTML;
         obj.text = note.querySelector('.note__text').innerHTML;
         obj.date = note.querySelector('.note__date').innerHTML;
-        obj.background=note.style.background;
-        obj.color=note.style.color;
-        if (note.querySelector('.note__favorit--active') != null)
-            {obj.favor = true;}
-        else obj.favor = false;
+        obj.background = note.style.background;
+        obj.color = note.style.color;
+        if (note.querySelector('.note__favorit--active') != null) {
+            obj.favor = true;
+        } else obj.favor = false;
         arr.push(obj);
-        
+
     }
     localStorage.setItem('arr', JSON.stringify(arr));
 
@@ -212,15 +213,16 @@ function saveNotes() {
 function loadNotes() {
     let arr = JSON.parse(localStorage.getItem('arr'));
     console.log(arr);
-    for (let i in arr){
-        if (i==0) {
-            let html=document.querySelector('html');
-            html.style.backgroundColor=arr[i].background;
-            html.style.color=arr[i].font;
-            document.querySelector('h1').innerHTML=arr[i].title;
-    continue};
-        addNote(arr[i].title, arr[i].text, arr[i].date, arr[i].favor,arr[i].background,arr[i].color);
-}
+    for (let i in arr) {
+        if (i == 0) {
+            let html = document.querySelector('html');
+            html.style.backgroundColor = arr[i].background;
+            html.style.color = arr[i].font;
+            document.querySelector('h1').innerHTML = arr[i].title;
+            continue
+        };
+        addNote(arr[i].title, arr[i].text, arr[i].date, arr[i].favor, arr[i].background, arr[i].color);
+    }
 }
 
 // Добавить Скрытие иконок заметки и появление либо активных, 
@@ -230,4 +232,3 @@ function loadNotes() {
 // 
 // 
 // 
-
